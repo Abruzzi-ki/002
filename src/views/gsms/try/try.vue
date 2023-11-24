577558dd3aff09dcbd7f2ef1e9d4cc756b820960
wangtx <1061003845@qq.com>
2023-11-24 10:29:01 +0800

    local-submit-001

---
+++ src/views/gsms/try/try.vue
@@ -0,0 +1,27 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+//import { } from
+export default {
+  data () {
+    return {
+
+    };
+  },
+  mounted () { },
+  watch: {
+    handler () {
+
+    }
+  },
+  methods: {},
+  components: {},
+  computed: {},
+  created () { },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
